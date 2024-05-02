00d692d37a30517b811750de61b70ae8281427ea
AlphaLeoXIII <108304634+AlphaLeoXIII@users.noreply.github.com>
2024-05-02 08:52:17 +0200

    Add files via upload

---
+++ CV/javascript.js
@@ -0,0 +1,5 @@
+const downloadBtn = document.querySelector(".Button")
+downloadBtn.addEventListener("click", () =>{
+    print();
+});
+//this code is for making the button responsive when clicked//
